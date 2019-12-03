e15a1826942565845b53dd441d106e9c7db199a5
Anisha Jain <anishajain@outlook.com>
2019-12-03 15:09:30 -0800

    api's implemented

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 // get nutrition info for one ingredient
-async function getNutrition(data) {
-    await fetch(`https://api.edamam.com/api/nutrition-details?app_id=96d0cce3&app_key=0b92e124b4e2945850af5faec0553fe2`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    })
-    .then(res => res.json())
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+async function getRecipeNutrition(app_id, app_key, data) {
+    // await fetch(`https://api.edamam.com/api/nutrition-details?app_id=${app_id}&app_key=${app_key}`, {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(data)
+    // })
+    // .then(res => res.json())
+    // .then(response => {
+    //     console.log(response);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // });
+
+    try {
+        const res = await fetch(`https://api.edamam.com/api/nutrition-details?app_id=${app_id}&app_key=${app_key}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        const json = await res.json()
+        return json
+    } catch(err) {
+        return err
+    } 
+}
+
+// returns a json promise or error
+async function getIngredientNutrition(app_id, app_key, ingr) {
+    ingr = ingr.replace(/ /g, "%20")
+    const url = `https://api.edamam.com/api/nutrition-data?app_id=${app_id}&app_key=${app_key}&ingr=${ingr}`
+    try {
+        const res = await fetch(url)
+        const json = await res.json()
+        return json
+    } catch(err) {
+        return err
+    } 
 }
 
